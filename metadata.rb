0b099e9664efea90c47a04ed98e38e76649b3fe6
Mandi Walls <mandi.walls@gmail.com>
2015-05-18 15:48:24 +0100

    initial commit

---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'sample'
+maintainer       'linuxchick.org'
+maintainer_email 'mandi@chef.io'
+license          'All rights reserved'
+description      'Installs/Configures sample'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
