9fe334ce6c0ae0fb13be61312fa624d7fe9347e4
Mandi Walls <mandi.walls@gmail.com>
2015-05-19 14:04:44 +0100

    bumping and changing name

--- metadata.rb
+++ metadata.rb
@@ -1,7 +1,7 @@
-name             'sample'
+name             'sample-cookbook'
 maintainer       'linuxchick.org'
 maintainer_email 'mandi@chef.io'
 license          'All rights reserved'
 description      'Installs/Configures sample'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.3'
+version          '0.1.4'
