9cd409bc95d370988f234d384fd97857c343f87d
Mandi Walls <mandi.walls@gmail.com>
2015-06-23 21:30:40 +0200

    bumping metadata

--- metadata.rb
+++ metadata.rb
@@ -4,5 +4,5 @@ maintainer_email 'mandi@chef.io'
 license          'All rights reserved'
 description      'Installs/Configures sample'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.8'
+version          '0.1.9'
 depends          'apt'
